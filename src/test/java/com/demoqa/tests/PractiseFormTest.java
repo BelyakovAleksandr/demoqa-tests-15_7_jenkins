c30d101693dfb2ddaf1de6f5ce5a7247e48f03bb
BelyakovAleksandr <113695437+BelyakovAleksandr@users.noreply.github.com>
2022-11-27 17:55:26 +0400

    demoqa-tests-15_7_jenkins Update

---
+++ src/test/java/com/demoqa/tests/PractiseFormTest.java
@@ -0,0 +1,46 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.PractiseFormPage;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class PractiseFormTest extends TestData {
+    PractiseFormPage practiseFormPage = new PractiseFormPage();
+
+    @Test
+    void practiceForm() {
+        step("Open registration form", () -> {
+            practiseFormPage.OpenForm();
+        });
+
+        step("Fill the form", () -> {
+            practiseFormPage.setName(firstName, lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setMobilePhone(mobilePhone)
+                    .setSubjects(subject)
+                    .setHobbies()
+                    .setCurrentAddress(currentAddress)
+                    .uploadPicture(file)
+                    .setState(state)
+                    .setCity(city)
+                    .setDate(day, month, year)
+                    .clickSubmit();
+        });
+
+        step("Check results on the table", () -> {
+            practiseFormPage.checkResults("Student Name", firstName)
+                    .checkResults("Student Name", lastName)
+                    .checkResults("Student Email", email)
+                    .checkResults("Gender", gender)
+                    .checkResults("Mobile", mobilePhone)
+                    .checkResults("Date of Birth", day + " " + month + "," + year)
+                    .checkResults("Subjects", subject)
+                    .checkResults("Hobbies", hobbies)
+                    .checkResults("Picture", file.getName())
+                    .checkResults("Address", currentAddress)
+                    .checkResults("State and City", state + " " + city);
+        });
+    }
+}
---
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -0,0 +1,54 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import com.github.javafaker.Faker;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+public class TestData {
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+ //       capabilities.setCapability("browserName", "chrome");
+//        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "390x844";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
+    Faker faker = new Faker();
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String gender = "Male";
+    String email = faker.internet().emailAddress();
+    String mobilePhone = faker.phoneNumber().subscriberNumber(10);
+    String subject = "Computer Science";
+    String hobbies = "Music";
+    String currentAddress = faker.address().fullAddress();
+    File file = new File("src/test/resources/Test_Demo.jpg");
+    String day = "20";
+    String month = "May";
+    String year = "1996";
+    String state = "NCR";
+    String city = "Noida";
+}
